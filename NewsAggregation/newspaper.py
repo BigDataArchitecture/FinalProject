979848a0f59ff114e7fd424b8cabeee7596b52be
Parth Shah <shah.parth3@northeastern.edu>
2022-04-23 14:49:33 -0400

    added files

--- NewsAggregation/newspaper.py
+++ NewsAggregation/newspaper.py
@@ -1,6 +1,6 @@
 import pymongo
 
-
+#
 client = pymongo.MongoClient("mongodb+srv://team3:qHovInc8WtqPBs7k@newsmonitor.uzcq9.mongodb.net/UserData?retryWrites=true&w=majority")
 print(client["UserData"])
 db = client["UserData"]
